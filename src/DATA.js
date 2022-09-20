fa3a8f19b73ab455326d0040345651662a0865cd
NikitaShevchik <shevchik.nikita@gmail.com>
2022-09-20 18:42:56 +0300

    DATA+

--- src/DATA.js
+++ src/DATA.js
@@ -52,6 +52,54 @@ export const DATA = [
         name: 'Air force 1',
         brand: 'Nike',
         price: 8900
+    },
+    {
+        id: id(),
+        img: 'https://cdn.shopify.com/s/files/1/0609/3016/2861/products/eds-store-sneakersYeezy_Slide_Pure-GZ5554-1_2000x.png?v=1643635225',
+        sex: 'male',
+        name: 'Yeezy slide pure',
+        brand: 'Adidas',
+        price: 12200
+    },
+    {
+        id: id(),
+        img: 'https://cdn.shopify.com/s/files/1/2358/2817/products/yeezy-foam-rnnr-sand-439236_1200x.png?v=1638814947',
+        sex: 'male',
+        name: 'Yeezy foam',
+        brand: 'adidas',
+        price: 24900
+    },
+    {
+        id: id(),
+        img: 'https://i.pinimg.com/originals/c1/58/05/c158056000436e80b3b084af89d7f5a9.png',
+        sex: 'male',
+        name: 'Blazer mid 77 vintage',
+        brand: 'Nike',
+        price: 14800
+    },
+    {
+        id: id(),
+        img: 'https://yzydealer.com/wp-content/uploads/Nike-Air-Force-1-Off-White-Black-2.png',
+        sex: 'male',
+        name: 'Air Force off white',
+        brand: 'Nike',
+        price: 78800
+    },
+    {
+        id: id(),
+        img: 'https://cdn.shopify.com/s/files/1/2358/2817/products/air-force-1-low-laser-orange-147714_1200x.png?v=1638812364',
+        sex: 'male',
+        name: 'Air Force 1 low laser orange',
+        brand: 'Nike',
+        price: 14900
+    },
+    {
+        id: id(),
+        img: 'http://cdn.shopify.com/s/files/1/0464/7685/3402/products/Wethenew-Sneakers-France-Nike-Air-Force-1-Shadow-Total-Orange-CI0919-103-2_2000x_0c70e956-d4e2-498f-8c26-86d3cc5f09b4_1200x1200.png?v=1598177103',
+        sex: 'female',
+        name: 'Air Force 1 shadow total orange',
+        brand: 'Nike',
+        price: 24500
     }
 
 ]
