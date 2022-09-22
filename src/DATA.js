2735255452a88b33d961791cd360292e489e3744
NikitaShevchik <shevchik.nikita@gmail.com>
2022-09-22 15:27:21 +0300

    json +

--- src/DATA.js
+++ src/DATA.js
@@ -102,3 +102,90 @@ export const DATA = [
         price: 24500
     },
 ]
+
+const dataNoId = [
+    {
+        img: 'https://www.yeezypop.com/wp-content/uploads/2019/07/by9612_3.png',
+        sex: 'male',
+        name: 'Blazer Mid Suede',
+        brand: 'Nike',
+        price: 12999
+    },
+    {
+        img: 'https://cdn.shopify.com/s/files/1/0609/3016/2861/products/eds-store-sneakersDunk_Low_Laser_Orange-DD1503-800-1_2000x.png?v=1643631628',
+        sex: 'male',
+        name: 'Air Max 270',
+        brand: 'Nike',
+        price: 12999
+    },
+    {
+        img: 'https://i.pinimg.com/originals/71/08/8c/71088c1a101e6f2ac0845545446bee60.png',
+        sex: 'male',
+        name: 'Yeezy 350',
+        brand: 'Adidas',
+        price: 12999
+    },
+    {
+        img: 'https://i.pinimg.com/564x/4a/1f/9a/4a1f9af983c79c96ef36352b96442c1f.jpg',
+        sex: 'female',
+        name: 'Govnuma 228',
+        brand: 'Puma',
+        price: 12999
+    },
+    {
+        img: 'https://cdn.shopify.com/s/files/1/2358/2817/products/air-jordan-1-retro-high-off-white-chicago-the-ten-563040_5000x.png?v=1638812979',
+        sex: 'male',
+        name: 'Air Jordan 11',
+        brand: 'Jordan',
+        price: 12999
+    },
+    {
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8yyYe0nv2Z0dsjwC6xYblY21XCOLEZtlUf6Y4Rmb_HLN61QqBb-o2zlf0X1r7PYmSSIc&usqp=CAU',
+        sex: 'male',
+        name: 'Air force 1',
+        brand: 'Nike',
+        price: 8900
+    },
+    {
+        img: 'https://cdn.shopify.com/s/files/1/0609/3016/2861/products/eds-store-sneakersYeezy_Slide_Pure-GZ5554-1_2000x.png?v=1643635225',
+        sex: 'male',
+        name: 'Yeezy slide pure',
+        brand: 'Adidas',
+        price: 12200
+    },
+    {
+        img: 'https://cdn.shopify.com/s/files/1/2358/2817/products/yeezy-foam-rnnr-sand-439236_1200x.png?v=1638814947',
+        sex: 'male',
+        name: 'Yeezy foam',
+        brand: 'adidas',
+        price: 24900
+    },
+    {
+        img: 'https://i.pinimg.com/originals/c1/58/05/c158056000436e80b3b084af89d7f5a9.png',
+        sex: 'male',
+        name: 'Blazer mid 77 vintage',
+        brand: 'Nike',
+        price: 14800
+    },
+    {
+        img: 'https://yzydealer.com/wp-content/uploads/Nike-Air-Force-1-Off-White-Black-2.png',
+        sex: 'male',
+        name: 'Air Force off white',
+        brand: 'Nike',
+        price: 78800
+    },
+    {
+        img: 'https://cdn.shopify.com/s/files/1/2358/2817/products/air-force-1-low-laser-orange-147714_1200x.png?v=1638812364',
+        sex: 'male',
+        name: 'Air Force 1 low laser orange',
+        brand: 'Nike',
+        price: 14900
+    },
+    {
+        img: 'http://cdn.shopify.com/s/files/1/0464/7685/3402/products/Wethenew-Sneakers-France-Nike-Air-Force-1-Shadow-Total-Orange-CI0919-103-2_2000x_0c70e956-d4e2-498f-8c26-86d3cc5f09b4_1200x1200.png?v=1598177103',
+        sex: 'female',
+        name: 'Air Force 1 shadow total orange',
+        brand: 'Nike',
+        price: 24500
+    },
+]
--- src/components/elements/Catalog/Catalog.jsx
+++ src/components/elements/Catalog/Catalog.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import { useState } from "react";
 import { DATA } from "../../../DATA";
 import Search from "../../UI/Search/Search";
 import CardItem from "../CardItem/CardItem";
+import { nanoid } from 'nanoid'
 import styles from './Catalog.module.scss'
+import { json } from "react-router-dom";
+import { useEffect } from "react";
+
+function id() {
+    return nanoid();
+}
 
 const Catalog = () => {
+
+    const [items, setItems] = useState([])
+    useEffect(() => {
+        fetch('https://632c38cd5568d3cad880126d.mockapi.io/items')
+            .then((res) => {
+                return res.json();
+            })
+            .then((json) => {
+                console.log(items)
+                setItems(json.map(item => {
+                    return { ...item, id: id() }
+                }))
+            })
+    }, []);
+
     return (
         <div className={styles.catalog}>
             <div className={styles.catalog__header}>
@@ -14,7 +37,7 @@ const Catalog = () => {
                 <Search />
             </div>
             <div className={styles.catalog__items}>
-                {DATA.map(product => {
+                {items.map(product => {
                     return (
                         <CardItem
                             key={product.id}
