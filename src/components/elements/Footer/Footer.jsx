7ade92ea285c13927821b1d9426728934dee8b91
NikitaShevchik <shevchik.nikita@gmail.com>
2022-09-28 22:42:20 +0300

    emoji to add footer

--- src/components/elements/Footer/Footer.jsx
+++ src/components/elements/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <div className={styles.footer}>
             <span>SHEVCHIK SNEAKERS by Nikita Shevchik</span>
-            <span>2022 BELARUS</span>
+            <span>2022 🇧🇾</span>
         </div>
     )
 }
