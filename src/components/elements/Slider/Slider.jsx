8a8d7f4d7b8f96f806aac3446ba0c595306747c6
NikitaShevchik <shevchik.nikita@gmail.com>
2022-10-04 19:37:48 +0300

    skeleton slider + axios slider

--- src/components/elements/Slider/Slider.jsx
+++ src/components/elements/Slider/Slider.jsx
@@ -3,6 +3,7 @@ import { GrFormNext, GrFormPrevious } from 'react-icons/gr';
 import { FcNext, FcPrevious } from 'react-icons/fc';
 import { nanoid } from "nanoid";
 import SliderBanner from "../../UI/SliderBanner/SliderBanner";
+import ContentLoader from "react-content-loader"
 import styles from './Slider.module.scss';
 import axios from "axios";
 
@@ -26,23 +27,38 @@ const Slider = () => {
         transform: `translate(-${numberSlider * 100}%, 0)`
     }
     return (
+
         <div className={styles.slider}>
             {numberSlider > 0 &&
                 <div className={styles.slider__prev} onClick={() => setNumberSlider(numberSlider - 1)}>
                     <GrFormPrevious />
                 </div>
             }
-            <div className={styles.slider__body}>
-                <div className={styles.slider__slider} style={styless}>
-                    {banners.map(banner => {
-                        return (
-                            <div className={styles.slider__banner} key={banner.id}>
-                                <SliderBanner key={banner.id} banner={banner} />
-                            </div>
-                        )
-                    })}
+            {banners.length > 0
+                ?
+                <div className={styles.slider__body}>
+                    <div className={styles.slider__slider} style={styless}>
+                        {banners.map(banner => {
+                            return (
+                                <div className={styles.slider__banner} key={banner.id}>
+                                    <SliderBanner key={banner.id} banner={banner} />
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
-            </div>
+                :
+                <ContentLoader
+                    speed={2}
+                    width={960}
+                    height={300}
+                    viewBox="0 0 960 300"
+                    backgroundColor="#f3f3f3"
+                    foregroundColor="#ecebeb"
+                >
+                    <rect x="0" y="0" rx="20" ry="20" width="960" height="300" />
+                </ContentLoader>
+            }
             {numberSlider < banners.length - 1 &&
                 <div className={styles.slider__next} onClick={() => setNumberSlider(numberSlider + 1)}>
                     <GrFormNext />
